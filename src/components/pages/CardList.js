28a2b4efe2b636deea2bbc40ab26336ef4be553f
Jordy J <j.janssen_14@hotmail.nl>
2019-10-07 12:01:55 +0200

    toevoeging van de cardlist
    todo list example geimplementeerd

--- src/components/pages/CardList.js
+++ src/components/pages/CardList.js
@@ -1,17 +1,68 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/layout/Header'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
+import axios from 'axios';
 
-class CardList extends Component {
+import './Overview.css';
+
+class CardList extends React.Component {
+    state = {
+        todos: []
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            .then(res => this.setState({ todos: res.data }))
+    }
+
+    // Toggle complete
+    markComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+        })
+    }
+
+    // Delete Todo
+    delTodo = id => {
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
+    }
+
+    //Add Todo
+    addTodo = (title) => {
+        axios.post('https://jsonplaceholder.typicode.com/todos', {
+            title: title,
+            completed: false
+        })
+            .then(res => this.setState({ todos: [...this.state.todos, res.data] }));
+    }
     render() {
         return (
-        //All of the content will be between the react fragments
-        <React.Fragment>
-            <h1>Card List</h1>
-            <p>
-                This is a cardlist that Jordy will create
-            </p>
-        </React.Fragment>
-        )
+            <Router>
+                <div className="App">
+                    <div className="container">
+                        <Header />
+                        <Route exact path="/" render={props => (
+                            <React.Fragment>
+                                <AddTodo addTodo={this.addTodo} />
+                                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                                    delTodo={this.delTodo} />
+                            </React.Fragment>
+                        )} />
+                        <Route path="/about" component={About} />
+                    </div>
+                </div>
+            </Router>
+        );
     }
+
 }
 
 export default CardList
