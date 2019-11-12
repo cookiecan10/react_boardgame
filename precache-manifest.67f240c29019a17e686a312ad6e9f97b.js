ab8779d3d5d108a430c3e263dd4ff96952a1860a
Cookiecan10 <Tijmenbekkering@hotmail.nl>
2019-11-12 15:24:17 +0100

    Updates

---
+++ precache-manifest.67f240c29019a17e686a312ad6e9f97b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da9cd06c36185222a7d8e84694b27e9f",
+    "url": "/react_boardgame/index.html"
+  },
+  {
+    "revision": "da11c430b255460f66f9",
+    "url": "/react_boardgame/static/css/main.e99f19b3.chunk.css"
+  },
+  {
+    "revision": "901a7ca80e1fef645544",
+    "url": "/react_boardgame/static/js/2.299ae726.chunk.js"
+  },
+  {
+    "revision": "da11c430b255460f66f9",
+    "url": "/react_boardgame/static/js/main.334261f6.chunk.js"
+  },
+  {
+    "revision": "ea2db21d6fdec2fd3815",
+    "url": "/react_boardgame/static/js/runtime-main.bfe1601a.js"
+  },
+  {
+    "revision": "79af7cb7bb2c5632d3ae31233a907244",
+    "url": "/react_boardgame/static/media/background.79af7cb7.png"
+  },
+  {
+    "revision": "57f5efa274b79ff8f6c6f30a637314b9",
+    "url": "/react_boardgame/static/media/bordspel_cirkel.57f5efa2.PNG"
+  }
+]);
